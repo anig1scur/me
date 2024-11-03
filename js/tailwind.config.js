ac3c1266648c2fc20ccb96e6152296951fc9d7f7
anig1scur <anigiscur@gmail.com>
2024-11-03 18:15:06 +0800

    chore: use light theme!

---
+++ js/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(8deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      backgroundImage: {
+        eunice: "url('./assets/eunice.png')",
+        paint: "url('./assets/painting.svg')",
+        project: "url('./assets/project.svg')",
+        travel: "url('./assets/travel.svg')",
+        memo: "url('./assets/memo.svg')",
+        avatar: "url('./assets/eunice_avatar.png')",
+      },
+    }
+  }
+}
